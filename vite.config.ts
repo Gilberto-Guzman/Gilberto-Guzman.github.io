764fe41edd14023761868b97bde95b6c2b252bbc
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-06-25 22:29:13 -0600

    estructuracion del proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const root = resolve(__dirname, 'src')
+const root = resolve(__dirname, 'src/templates/views/')
 const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
@@ -15,7 +15,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(root, 'index.html'),
+        home: resolve(root, 'index.html'),
         about: resolve(root, 'about', 'index.html')
       }
     }
