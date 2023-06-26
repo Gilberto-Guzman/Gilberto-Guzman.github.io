05d66e633589b6bf6788f44435a015a5a55a2459
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-06-25 22:35:13 -0600

    testeando funcionalidades

---
+++ src/templates/views/home/container/test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import Carousel from "react-material-ui-carousel";
+
+const images = [
+    {
+        image: 'https://via.placeholder.com/500x300',
+        caption: 'Image 1',
+    },
+    {
+        image: 'https://via.placeholder.com/500x300',
+        caption: 'Image 2',
+    },
+    {
+        image: 'https://via.placeholder.com/500x300',
+        caption: 'Image 3',
+    },
+];
+
+const App: React.FC = () => {
+    return (
+        <Box sx={{ margin: 5 }} >
+            <Carousel animation="slide" >
+                {images.map((item, index) => (
+                    <Box key={index}>
+                        <img src={item.image} alt={`Image ${index + 1}`} style={{ width: '100%' }} />
+                    </Box>
+                ))}
+            </Carousel>
+        </Box >
+    );
+};
+
+export default App;
--- src/templates/views/home/home.tsx
+++ src/templates/views/home/home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Navbar from '../../components/Navbar';
 
+import Test from './container/test';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>HOME</h1>
+    <Navbar />
+    <Test />
   </React.StrictMode>,
 )
