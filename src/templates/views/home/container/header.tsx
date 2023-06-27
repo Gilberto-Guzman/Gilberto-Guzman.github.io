216983d40bfcdf159f23395562d677dbbfb3a104
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-06-27 11:55:50 -0600

    testeando funcionalidad

--- src/templates/views/home/container/header.tsx
+++ src/templates/views/home/container/header.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Box, Typography } from '@mui/material'
 import Paper from '@mui/material/Paper';
+import Zoom from '@mui/material/Zoom';
+
+
+
 const Header: React.FC = () => {
     return (
         <Box sx={{ textAlign: 'center', margin: 5 }}>
-            <Paper elevation={6}>
-                <Typography variant="h1" gutterBottom>
-                    Hi, I'm Gilbert ☕
-                </Typography>
-                <Typography variant="h4" gutterBottom>
-                    I'm a Software Enginner student, I'm interested in everything related to technology and sotfware development.
-                </Typography>
-            </Paper>
-        </Box>
+            <Zoom in={true} style={{ transitionDelay: '500ms' }}>
+                <Paper elevation={6}>
+                    <Typography variant="h1" gutterBottom>
+                        Hi, I'm Gilbert ☕
+                    </Typography>
+                    <Typography variant="h4" gutterBottom>
+                        I'm a Software Enginner student, I'm interested in everything related to technology and sotfware development.
+                    </Typography>
+                </Paper>
+            </Zoom>
+        </Box >
     )
 }
 
