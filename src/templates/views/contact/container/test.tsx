63b291cfd3764e7c4b17ff8d437d2f9772c8f776
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-07-22 21:22:52 -0600

    testeando funcionalidad

--- src/templates/views/contact/container/test.tsx
+++ src/templates/views/contact/container/test.tsx
@@ -22,36 +22,60 @@ import {
 
 /* --- Importacion de ARCHIVOS --- */
 
-import pythonLogo from '../../../public/images/knowledge/code/python.png';
-import cLogo from '../../../public/images/knowledge/code/c.png';
-import cppLogo from '../../../public/images/knowledge/code/cpp.png';
-import csharpLogo from '../../../public/images/knowledge/code/csharp.png';
-import htmlLogo from '../../../public/images/knowledge/code/html.png';
-import markdownLogo from '../../../public/images/knowledge/code/markdown.png';
-import cssLogo from '../../../public/images/knowledge/code/css.png';
-import javascriptLogo from '../../../public/images/knowledge/code/javascript.png';
-import phpLogo from '../../../public/images/knowledge/code/php.png';
+import officeLogo from '../../../public/images/knowledge/ability/office.png';
+import filmoraLogo from '../../../public/images/knowledge/ability/filmora.png';
+import canvaLogo from '../../../public/images/knowledge/ability/canva.png';
+import preziLogo from '../../../public/images/knowledge/ability/prezi.png';
+import vismeLogo from '../../../public/images/knowledge/ability/visme.png';
 
-/* --- Gestion de los nombres y enlaces de code --- */
 
-const codes = [
-    { name: 'Python', link: pythonLogo },
-    { name: 'C', link: cLogo },
-    { name: 'C++', link: cppLogo },
-    { name: 'C#', link: csharpLogo },
-    { name: 'HTML', link: htmlLogo },
-    { name: 'Markdown', link: markdownLogo },
-    { name: 'CSS', link: cssLogo },
-    { name: 'JavaScript', link: javascriptLogo },
-    { name: 'PHP', link: phpLogo }
+/* --- Gestion de los nombres y enlaces de ability --- */
+
+const abilities = [
+    { name: 'Microsoft Office', link: officeLogo },
+    { name: 'Filmora', link: filmoraLogo },
+    { name: 'Canva', link: canvaLogo },
+    { name: 'Prezi', link: preziLogo },
+    { name: 'Visme', link: vismeLogo }
 ];
 
-export default function NestedList() {
+export default function Ability() {
+    // cambia a false para ocultar la lista
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
 
     return (
-        <Slide in={true} direction="up" style={{ transitionDelay: '300ms' }}>
+        <Slide in={true} direction="up" style={{ transitionDelay: '800ms' }}>
             <Paper elevation={6}>
-                <h1>gsadfsfd</h1>
+                <List
+                    sx={{ margin: 2 }}
+                    component="nav"
+                    aria-labelledby="nested-list-subheader"
+                >
+                    <ListItemButton onClick={handleClick}>
+                        <ListItemText primary={<Typography variant="h5" gutterBottom>
+                            Other Abilities
+                        </Typography>} />
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+
+                            {abilities.map((ability) => (
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemIcon>
+                                        <Avatar src={ability.link} />
+                                    </ListItemIcon>
+                                    <ListItemText primary={ability.name} />
+                                </ListItemButton>
+                            ))}
+
+                        </List>
+                    </Collapse>
+                </List>
             </Paper>
         </Slide >
     );
