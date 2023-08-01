a0407c902594b99ee71c27ae9d6b21cecc251179
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-07-31 18:25:07 -0600

    testeando funcionalidad

--- src/templates/views/contact/container/test.tsx
+++ src/templates/views/contact/container/test.tsx
@@ -8,57 +8,51 @@ import {
     Button
 } from '@mui/material';
 
-import PhoneIcon from '@mui/icons-material/Phone';
+
+/* --- Importacion de ICONOS --- */
+import {
+    Phone
+} from '@mui/icons-material';
 
 /* --- Gestion de ... --- */
 
-// const contacts = [
-//     { contact: 'Phone', info: '(221)529-2048', link: 'tel:+2215292048' },
-// ];
+const contacts = [
+    { icon: <Phone sx={{ fontSize: 60 }} />, media: 'Phone', info: '(221)529-2048', link: 'tel:+2215292048', button: 'Start a call' },
+];
 
 export default function Test() {
     return (
-        <Slide in={true} direction='up' style={{ transitionDelay: '300ms' }}>
-            <Box sx={{ flexGrow: 1, mt: 2 }}>
+
+        <Box sx={{ flexGrow: 1, mt: 2 }}>
+            <Slide in={true} direction='up' style={{ transitionDelay: '300ms' }}>
+
                 <Grid container spacing={2} justifyContent='center'>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Paper elevation={6}>
-                            <Box sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
-                                <Avatar sx={{ width: 100, height: 100, backgroundColor: '#3D4043' }}>
-                                    <PhoneIcon sx={{ fontSize: 60 }} />
-                                </Avatar>
-                            </Box>
-                            <Typography variant='h4' textAlign='center' sx={{ fontFamily: 'monospace', fontWeight: 700, color: 'inherit' }}>
-                                Phone
-                            </Typography>
-                            <Typography variant='h6' textAlign='center'>
-                                (221)529-2048
-                            </Typography>
-                            <Box sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
-                                <Button href='tel:+2215292048' variant="contained" color="inherit">
-                                    <Typography variant='h6' sx={{ fontFamily: 'monospace', fontWeight: 700, color: 'inherit' }}>
-                                        Start a call
-                                    </Typography>
-                                </Button>
-                            </Box>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Paper elevation={6}>
-                            <Typography variant='h5' align='center' gutterBottom>
-                                Correo Electrónico
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Paper elevation={6}>
-                            <Typography variant='h5' align='center' gutterBottom>
-                                Github
-                            </Typography>
-                        </Paper>
-                    </Grid>
+                    {contacts.map((contact) => (
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Paper elevation={6}>
+                                <Box sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
+                                    <Avatar sx={{ width: 100, height: 100, backgroundColor: '#3D4043' }}>
+                                        {contact.icon}
+                                    </Avatar>
+                                </Box>
+                                <Typography variant='h4' textAlign='center' sx={{ fontFamily: 'monospace', fontWeight: 700, color: 'inherit' }}>
+                                    {contact.media}
+                                </Typography>
+                                <Typography variant='h6' textAlign='center'>
+                                    {contact.info}
+                                </Typography>
+                                <Box sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
+                                    <Button href={contact.link} variant="contained" color="inherit">
+                                        <Typography variant='h6' sx={{ fontFamily: 'monospace', fontWeight: 700, color: 'inherit' }}>
+                                            Start a call
+                                        </Typography>
+                                    </Button>
+                                </Box>
+                            </Paper>
+                        </Grid>
+                    ))}
                 </Grid>
-            </Box>
-        </Slide >
+            </Slide >
+        </Box >
     );
 }
