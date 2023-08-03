c4fe97dc428ebb2b72408bdae50208dd1aab2981
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-08-02 18:08:54 -0600

    correcion de errores

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 const root = resolve(__dirname, 'src/templates/views/')
-const outDir = resolve(__dirname, 'dist/templates/views/')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
