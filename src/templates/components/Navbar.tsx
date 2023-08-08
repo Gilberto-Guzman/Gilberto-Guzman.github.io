a78eed53972e57356d0b02fe01b672964fefdb50
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-08-08 12:12:37 -0600

    testeando funcionalidad

--- src/templates/components/Navbar.tsx
+++ src/templates/components/Navbar.tsx
@@ -2,23 +2,37 @@
 
 import * as React from 'react';
 
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  Button,
-  IconButton,
-  Typography,
-  Menu,
-  Container,
-  Avatar,
-  Tooltip,
-  MenuItem,
-  Link
-} from "@mui/material";
+// import {
+//   AppBar,
+//   Box,
+//   Toolbar,
+//   Button,
+//   IconButton,
+//   Typography,
+//   Menu,
+//   Container,
+//   Avatar,
+//   Tooltip,
+//   MenuItem,
+//   Link
+// } from "@mui/material";
 
+// import MenuIcon from '@mui/icons-material/Menu';
+
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Link from '@mui/material/Link';
 
 /* --- Importacion de ARCHIVOS --- */
 
