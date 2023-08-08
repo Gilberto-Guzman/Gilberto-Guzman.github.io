7a0c3f5cbba9ebfe04f1857e39c2e476bc9618de
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-08-08 12:09:04 -0600

    testeando funcionalidad

--- src/templates/components/Navbar.tsx
+++ src/templates/components/Navbar.tsx
@@ -20,20 +20,6 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 
 
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-// import Menu from '@mui/material/Menu';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import Container from '@mui/material/Container';
-// import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import Tooltip from '@mui/material/Tooltip';
-// import MenuItem from '@mui/material/MenuItem';
-// import Link from '@mui/material/Link';
-
 /* --- Importacion de ARCHIVOS --- */
 
 import avatarImage from '../public/images/home/avatar.jpeg';
