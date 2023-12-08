58928908eb4ff6c6d076fe571770f03f7cbf8161
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-12-07 23:31:35 -0600

    Correcion de warning key

--- src/templates/views/contact/container/info.tsx
+++ src/templates/views/contact/container/info.tsx
@@ -21,27 +21,27 @@ import {
 /* --- Gestion del delay, iconos, medios de comunicación, información de contacto, enlaces y botones --- */
 
 const contacts = [
-    { 
-        delay: '0ms', 
-        icon: <Phone sx={{ fontSize: 60 }} />, 
-        media: 'Phone', info: '(221)529-2048', 
-        link: 'tel:+2215292048', 
-        button: 'Start a call' 
+    {
+        delay: '0ms',
+        icon: <Phone sx={{ fontSize: 60 }} />,
+        media: 'Phone', info: '(221)529-2048',
+        link: 'tel:+2215292048',
+        button: 'Start a call'
     },
-    { 
-        delay: '100ms', 
-        icon: <Email sx={{ fontSize: 60 }} />, 
-        media: 'E-Mail', info: 'josegilbertoguzmangutierrez@gmail.com', 
-        link: 'mailto:josegilbertoguzmangutierrez@gmail.com', 
-        button: 'Send me an email' 
+    {
+        delay: '100ms',
+        icon: <Email sx={{ fontSize: 60 }} />,
+        media: 'E-Mail', info: 'josegilbertoguzmangutierrez@gmail.com',
+        link: 'mailto:josegilbertoguzmangutierrez@gmail.com',
+        button: 'Send me an email'
     },
-    { 
-        delay: '150ms', 
-        icon: <GitHub sx={{ fontSize: 60 }} />, 
-        media: 'Github', 
-        info: 'https://github.com/Gilberto-Guzman', 
-        link: 'https://github.com/Gilberto-Guzman', 
-        button: 'Check my lastest work' 
+    {
+        delay: '150ms',
+        icon: <GitHub sx={{ fontSize: 60 }} />,
+        media: 'Github',
+        info: 'https://github.com/Gilberto-Guzman',
+        link: 'https://github.com/Gilberto-Guzman',
+        button: 'Check my lastest work'
     }
 ];
 
@@ -50,8 +50,8 @@ export default function Info() {
 
         <Box sx={{ flexGrow: 1, mt: 2 }}>
             <Grid container spacing={2} justifyContent='center'>
-                {contacts.map((contact) => (
-                    <Zoom in={true} style={{ transitionDelay: contact.delay }}>
+                {contacts.map((contact, index) => (
+                    <Zoom in={true} style={{ transitionDelay: contact.delay }} key={index}>
                         <Grid item xs={12} sm={6} md={4}>
                             <Paper elevation={6}>
                                 <Box sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
