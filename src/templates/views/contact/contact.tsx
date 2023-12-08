f1911ad05bddf2794b0c7399b0aa0fb645cc6c76
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-12-07 23:31:22 -0600

    actualizacion ReactDOM

--- src/templates/views/contact/contact.tsx
+++ src/templates/views/contact/contact.tsx
@@ -1,18 +1,16 @@
 /* --- Importacion de LIBRERIAS --- */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 /* --- Importacion de ARCHIVOS --- */
 
 import Navbar from '../../components/Navbar';
 import Info from './container/info';
 
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Navbar />
         <Info />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 )
